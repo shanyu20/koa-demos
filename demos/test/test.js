321e9408b432631f7ee65a7cf0ea1e4bcf31e9ac
cjb <jiangbo.chen@fondalighting.com>
2024-06-03 17:26:52 +0800

    feat(test): 增加表单测试

--- demos/test/test.js
+++ demos/test/test.js
@@ -18,7 +18,25 @@ const clock = ctx => {
   ctx.response.body = fs.createReadStream('./clock.html')
 }
 
+const form = ctx => {
+  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
+  if  (ctx.request.querystring) {
+    ctx.response.type = 'json';
+    ctx.response.body = {
+      url: ctx.request.url,
+      ctr_query: ctx.request.query,
+      ctx_querystring: ctx.request.querystring
+    }
+  } else {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('./form.html')
+  }
+}
+
 app.use(route.get('/', main));
 app.use(route.get('/hello', hello));
 app.use(route.get('/clock', clock));
-app.listen(3000);
+app.use(route.get('/form', form))
+app.listen(3000, ()=>
+  console.log('App started on http://localhost:3000')
+);
